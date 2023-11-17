a8be96dcb616934148d0b91ab9205ceab3881d69
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-17 22:50:25 +0100

    documentation for class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 # models/base.py
+'''Module for Base class.'''
 
 class Base:
+    '''A representation of the base of our OOP hierarchy.'''
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+         '''Constructor.'''
+         
         if id is not None:
             self.id = id
         else:
