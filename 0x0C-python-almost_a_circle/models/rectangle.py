1cc06d4c9c6440ee69c855077906a564b488e8d8
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-18 01:42:27 +0100

    class Rectangle -validation error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,11 @@
 from models.base import Base
 
 class Rectangle(Base):
-    '''Class representing a rectangle inhirite Base class'''
+    '''Rectangle class inhirite from Base class'''
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
+        '''constructor: call of the parent construct'''
+
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,7 +18,6 @@ class Rectangle(Base):
     @property
     def width(self):
         '''Width getter of this rectangle.'''
-
         return self.__width
 
     @width.setter
@@ -24,9 +25,9 @@ class Rectangle(Base):
         '''Width setter of this rectangle.'''
 
         if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Width must be greater than zero")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,35 +41,39 @@ class Rectangle(Base):
         '''height setter of this rectangle.'''
 
         if not isinstance(value, int):
-            raise TypeError("Height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("Height must be greater than zero")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
         '''x'''
+
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''Setter for the x coordinate of the top-left corner of the rectangle.'''
+        '''x setter'''
+
         if not isinstance(value, int):
-            raise TypeError("X must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("X cannot be negative")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
         '''y'''
+
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''Setter for the y coordinate of the top-left corner of the rectangle.'''
+        '''y setter'''
+        
         if not isinstance(value, int):
-            raise TypeError("Y must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("Y cannot be negative")
+            raise ValueError("y must be >= 0")
         self.__y = value
