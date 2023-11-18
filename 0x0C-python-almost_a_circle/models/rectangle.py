3bfafee6418f7693f5f277166403775baf82e8a8
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-18 02:58:51 +0100

    class Rectangle: add Update  method , tested

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,15 +91,12 @@ class Rectangle(Base):
         '''Return a string representation of the Rectangle.'''
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
-    def update(self, *args):
-        '''Assign arguments to attributes in order: id, width, height, x, y.'''
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        '''Assign arguments to attributes using both positional and keyworded arguments.'''
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
