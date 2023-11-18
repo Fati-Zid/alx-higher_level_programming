aeb630e4cc33746e31512794f2256be99962dd7c
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-18 03:54:56 +0100

    BaseClass: add save to file  method , tested

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,17 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''Return the JSON string representation of list_dictionaries.'''
-        
+
         if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Write the JSON string representation of list_objs to a file.'''
+        
+        if list_objs is None:
+            list_objs = []
+        filename = f"{cls.__name__}.json"
+        with open(filename, mode='w', encoding='utf-8') as file:
+            file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,9 @@
 '''Module for Base unit tests.'''
 import unittest
 from models.base import Base
+import os
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     '''Tests the Base class.'''
@@ -81,12 +84,55 @@ class TestBase(unittest.TestCase):
 
     def test_to_json_string_returns_json_representation_of_list_with_mixed_objects(self):
         '''Test returns json representation of list with mixed objects if valid list is passed'''
-        
+
         rectangle_instance = Rectangle(5, 4, 2, 3, 1)
         square_instance = Square(5, 2, 3, 2)
         result = Base.to_json_string([rectangle_instance.to_dictionary(), square_instance.to_dictionary()])
         expected = '[{"id": 1, "width": 5, "height": 4, "x": 2, "y": 3}, {"id": 2, "size": 5, "x": 2, "y": 3}]'
         self.assertEqual(result, expected)
+
+        def test_rectangle_json_created_for_none(self):
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        Rectangle.save_to_file(None)
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+    def test_rectangle_json_created_for_empty_list(self):
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        Rectangle.save_to_file([])
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+    def test_rectangle_json_created_for_list(self):
+        ''' created'''
+
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        rectangles = [Rectangle(3, 4), Rectangle(5, 8, 1), Rectangle(9, 1, 3, 2)]
+        Rectangle.save_to_file(rectangles)
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+    def test_rectangle_json_created_and_overwrite(self):
+        ''' created and overwrite'''
+
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        rectangles = [Rectangle(3, 4), Rectangle(5, 8, 1), Rectangle(9, 1, 3, 2)]
+        Rectangle.save_to_file(rectangles)
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+        # Overwrite the existing file
+        rectangles = [Rectangle(2, 2), Rectangle(4, 6, 2), Rectangle(8, 2, 1, 1)]
+        Rectangle.save_to_file(rectangles)
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+    def test_square_json_created_for_none(self):
+        '''created for none'''
+        
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
+        Square.save_to_file(None)
+        self.assertTrue(os.path.exists("Square.json"))
 
 if __name__ == '__main__':
     unittest.main()
