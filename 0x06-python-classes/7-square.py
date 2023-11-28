aba909afa28ab969a1ec6971cac56775a71ed430
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-28 02:16:04 +0100

    7-square: added

---
+++ 0x06-python-classes/7-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+
+        Raises:
+            TypeError: If `size` is not an integer or `position` is not a tuple of 2 integers.
+            ValueError: If `size` is less than 0.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square.
+
+        Args:
+            value (tuple): The position of the square.
+
+        Raises:
+            TypeError: If `value` is not a tuple of 2 integers.
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value):
+            raise TypeError("position must be a tuple of 2 integers")
+        self.__position = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square."""
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end='')
+            print("#" * self.__size)
