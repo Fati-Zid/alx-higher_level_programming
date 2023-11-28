e524a37178771bc18f5461bbdcbe6446474aaa36
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-11-28 01:52:52 +0100

    3-square: added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+File: 3-square.py
+"""
+
+class Square:
+    """
+    Defines a square with a private instance attribute size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
