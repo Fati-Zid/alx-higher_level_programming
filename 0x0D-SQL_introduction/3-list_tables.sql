05b2925bb8bb0fb7ec9f8a1d0f62a422b613390b
Fati-Zid <ezzaidanifatimazohra@gmail.com>
2023-12-14 13:58:39 -0800

    list tables: u2

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,3 @@
--- List all tables in the specified database (hbtn_0c_0 in this case)
-USE hbtn_0c_0;
--- Show tables without using SELECT or SHOW
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0';
+-- List all TABLES from database: mysql
+SHOW TABLES;
 
