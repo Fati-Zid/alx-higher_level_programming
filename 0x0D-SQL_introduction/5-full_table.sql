5ebf67e93f4749d6ae832b64913b1fef8b3e205c
Fati-Zid <ezzaidanifatimazohra@gmail.com>
2023-12-14 15:05:42 -0800

    describe tables: u

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
--- Display the full description of the first_table
-DESCRIBE hbtn_0c_0.first_table;
+-- Display the full description of the first_table without DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 
