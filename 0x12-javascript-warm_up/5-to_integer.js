dd4426b424e9573766cf0b55bd6ed55d34323726
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 00:45:55 +0100

    is number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-// is an integer
-
-if (process.argv[2] === undefined || typeof parseInt(process.argv[2]) != Number)
+// Is it a number
+const args = process.argv.slice(2);
+if (args.length < 1) {
   console.log('Not a number');
-else console.log('My number: ', process.argv[2]);
+} else if (isNaN(parseInt(args[0]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: '.concat(parseInt(args[0])));
+}
