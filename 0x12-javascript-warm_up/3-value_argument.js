b0006a537a6a100a504fedcfa4b86931e0074c66
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 00:13:29 +0100

    first arg value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // value of First argument
 
-if (process.argv[2] === undefined) console.log('No argument');
-else console.log(process.argv[2]);
+if (process.argv[2] === undefined) console.log(process.argv[2]);
+else console.log('No argument');
