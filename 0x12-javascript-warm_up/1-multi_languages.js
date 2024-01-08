6e2ac4dc66b9216664198e1a58230e1e82c3ce91
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-08 22:49:42 +0100

    fix:rrequerment

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // print 3 phrases in one console.log(), don't use var.
 
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+console.log('C is fun\n', 'Python is cool\n', 'JavaScript is amazing');
