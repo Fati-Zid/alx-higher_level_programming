4132fa3ecbb445d05c653ce902b5ed6b51cbc630
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 00:45:12 +0100

    is number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // is an integer
 
-if (typeof (parseInt(process.argv[2]) != Number)) console.log('Not a number');
+if (process.argv[2] === undefined || typeof parseInt(process.argv[2]) != Number)
+  console.log('Not a number');
 else console.log('My number: ', process.argv[2]);
