5768ba20164d163ab0a9b3efc4d7cbcf663f1ce4
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-08 23:06:04 +0100

    argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Print flag depend on argument.
+const count = process.argv.length;
+
+if (count < 3) console.log('No argument');
+else if (count == 3) console.log('Argument found');
+else console.log('Arguments found');
