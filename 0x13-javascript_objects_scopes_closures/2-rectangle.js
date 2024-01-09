1f64cbb9274e95109e431a4f8b1e803b78a5d62e
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 22:38:59 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle with constructor and check if argument was empty
 module.exports = class Rectangle {
-  // constructor  take 2 arguments w and h
+	// constructor  take 2 arguments w and h
 	constructor (w, h) {
 		if (w > 0 && h > 0) {
 			this.width = w;
