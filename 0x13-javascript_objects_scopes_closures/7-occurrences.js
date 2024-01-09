cb2fccb1c1c13e9db50a82c2c66fa1560298ccdd
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 23:55:48 +0100

    number of occur

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// function that returns the number of occurrences in a list
+module.exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for(let i=0;i<list.length;i++){
+        if(list[i]===searchElement){
+            count++;
+        }
+    }
+    return count;
+}
