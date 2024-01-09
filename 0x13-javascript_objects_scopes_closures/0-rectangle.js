244b0a4e2cfa1930d05a4e32628763214a8c1178
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 21:12:48 +0100

    add readme and rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//class Rectangle that defines a rectangle
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,4 @@
+# 0x13. JavaScript - Objects, Scopes and Closures
+
+** Author: Fati-Zid
+** GitHub: https://github.com/Fati-Zid
