b5b1e546bc52798db09aee1c77fd5eb42c238642
Fati-Zid <ezzaidanif@gmail.com.com>
2024-01-09 14:34:34 -0800

    fix standar

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //  square and inherits from Rectangle
-module.exports = class Square extends  require('./4-rectangle') {
-    constructor (size) {
-        super (size,size);
-    }
-}
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+};
