0a0cb0cd5d8f23776e73557e70062d05f10b6c53
Fati-Zid <ezzaidanif@gmail.com.com>
2024-01-09 15:01:59 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list
 module.exports.esrever = function (list) {
-    // check if input is an array, return error message if not
-    if (!Array.isArray(list)) {
-        throw new Error('Input should be an array');
-        }
-    let result = [];  // create empty array to store the reversed list in
-    for (let i = list.length -1; i >=0 ;i-- ) {
-        result.push(list[i]);   // add each element from last to first to the "result" array
-        };
-        return result;
-}
-
+  // check if input is an array, return error message if not
+  if (!Array.isArray(list)) {
+    throw new Error('Input should be an array');
+  }
+  const result = []; // create empty array to store the reversed list in
+  for (let i = list.length - 1; i >= 0; i--) {
+    result.push(list[i]); // add each element from last to first to the "result" array
+  }
+  return result;
+};
