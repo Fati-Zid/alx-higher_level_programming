14e9f8d14f12c27090591c9271eed153ad776b61
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-10 00:11:32 +0100

    logMe

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // function that prints the number of arguments already printed and the new argument value.
+logsCount = 0;
 module.exports.logMe = function (item) {
   console.log(this.logsCount + ': ' + this.item);
-  this.logsCount++;
+  logsCount++;
 };
