1f41efbe0e7ed9642882cbc53b577b9f5cabd966
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 22:01:54 +0100

    update rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//class Rectangle that defines a rectangle with constructor and check if argument was empty
+module.exports = class Rectangle {
+  //constructor  take 2 arguments w and h
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      obj = {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
