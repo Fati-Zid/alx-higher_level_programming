fdcbb64495b218f35d8e7a6ee2673c2569b55649
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 22:30:36 +0100

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-//class Rectangle that defines a rectangle with constructor
+// class Rectangle that defines a rectangle with constructor
 module.exports = class Rectangle {
-  //constructor  take 2 arguments w and h
-  constructor(w, h) {
+  // constructor  take 2 arguments w and h
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
