034de4eefe7d1fbef47edeca9a2dfd5ab04d69b8
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-09 23:47:33 +0100

    square: print C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+//  square and inherits from Square
+module.exports = class Square extends require('./5-square') {
+  // 
+  charPrint(c){
+    if (c == undefined)
+        this.print()
+    else
+        for (let index = 0; index < this.height; index++) 
+            console.log("c".repeat(this.width))
+  }
+};
