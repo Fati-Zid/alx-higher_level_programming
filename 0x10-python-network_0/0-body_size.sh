e4a772d0b510d656d823d549658565b08122118e
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-31 00:49:43 +0100

    refactor 0 body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request using curl, and displays the size of the response body in bytes.
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL=$1
-
-# Use curl to send a GET request and display the size of the response body in bytes
-SIZE=$(curl -sI "$URL" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
-
-if [ -z "$SIZE" ]; then
-  echo "Error: Unable to retrieve content length."
-else
-  echo "Correct output: GET $URL => \"$SIZE bytes\""
-fi
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
