aa9df1c6696d87de2352938956550c0eaa9551b2
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 22:32:16 +0100

    add exec

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3], charset="utf8")
+        # Connect to MySQL database
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
         cur = db.cursor()
+
+        # Execute query to select all states
         cur.execute("SELECT * FROM states ORDER BY id ASC")
         rows = cur.fetchall()
-        if not rows:
-            print()
-        else:
+
+        # Print states if there are any
+        if rows:
             for row in rows:
                 print(row)
+        else:
+            print()
+
+        # Close cursor and database connection
         cur.close()
         db.close()
+
     except MySQLdb.Error as e:
         print("MySQLdb Error:", e)
