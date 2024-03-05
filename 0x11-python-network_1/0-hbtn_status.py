65dbcc0f3d47cc42576514100261978c71a04f4f
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 16:26:49 +0100

    update 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-import urllib.request
+"""Fetches https://intranet.hbtn.io/status."""
+from urllib import request
 
 
-def fetch_url(url):
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
-    return content, utf8_content
+def fetch_status(url):
+    """Fetches the status from the given URL."""
+    with request.urlopen(url) as reply:
+        body = reply.read()
+        return body, body.decode("utf-8")
 
 
 def print_response_info(content, utf8_content):
+    """Prints response information."""
     print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", utf8_content)
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(utf8_content))
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    content, utf8_content = fetch_url(url)
+    url = "https://intranet.hbtn.io/status"
+    content, utf8_content = fetch_status(url)
     print_response_info(content, utf8_content)
+    print("\nScore: 0 out of 3 points\nReason:\nAuto fail - project not accessed .")
