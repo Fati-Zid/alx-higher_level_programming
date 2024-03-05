22ec8e2aee0ca0a8c441d10664608368848a0e9a
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 16:22:47 +0100

    update 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-from urllib import request
+import urllib.request
+
+
+def fetch_url(url):
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+    return content, utf8_content
+
+
+def print_response_info(content, utf8_content):
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", utf8_content)
 
 
 if __name__ == "__main__":
-	with request.urlopen("https://alx-intranet.hbtn.io/status") as reply:
-		body = reply.read()
-		print("Body response:")
-		print("\t- type: {}".format(type(body)))
-		print("\t- content: {}".format(body))
-		print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = 'https://alx-intranet.hbtn.io/status'
+    content, utf8_content = fetch_url(url)
+    print_response_info(content, utf8_content)
