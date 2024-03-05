60c7938ef19744ef4ad127926a21732b316be8c8
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 16:35:35 +0100

    update 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-from urllib import request
-
-
-def fetch_status(url):
-    """Fetches the status from the given URL."""
-    with request.urlopen(url) as reply:
-        body = reply.read()
-        return body, body.decode("utf-8")
-
-
-def print_response_info(content, utf8_content):
-    """Prints response information."""
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(utf8_content))
+import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    content, utf8_content = fetch_status(url)
-    print_response_info(content, utf8_content)
-    print("\nScore: 0 out of 3 points\nReason:\nAuto fail - project not accessed .")
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
