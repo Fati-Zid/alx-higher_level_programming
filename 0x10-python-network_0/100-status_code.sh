fab988277083b36c6700d24a4dc3c2dabf06b69b
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 23:11:35 +0100

    tst\

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument, and displays only the status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# script that sends a request to a URL passed as an argument
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# Bash script that sends a DELETE request to the URL
 curl -sX DELETE "$1"
