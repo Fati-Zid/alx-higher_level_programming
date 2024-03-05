04798a0fa0a66dcab88682bdc9df0176385ae6fa
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 21:16:15 +0100

    updates

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,7 +10,13 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        x_request_id = dict(response.headers).get("X-Request-Id")
-        if x_request_id:
-            print(x_request_id)
+    try:
+        with urllib.request.urlopen(request) as response:
+            x_request_id = response.getheader("X-Request-Id")
+            if x_request_id:
+                print(x_request_id)
+    except urllib.error.HTTPError as e:
+        if hasattr(e, 'headers') and 'X-Request-Id' in dict(e.headers):
+            print(e.headers['X-Request-Id'])
+        else:
+            print("No X-Request-Id found in HTTP headers.")
