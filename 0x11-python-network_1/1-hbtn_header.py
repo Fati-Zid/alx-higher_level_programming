4dafabce330efc888c352dff8b9b8ca910bc49e4
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 21:01:31 +0100

    refacto: 1-hbtn-header updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,18 +8,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    # Check if the URL argument is provided
-    if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        # Check if the X-Request-Id header is present
         x_request_id = response.headers.get("X-Request-Id")
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id not found in the HTTP header")
+        print(x_request_id)
