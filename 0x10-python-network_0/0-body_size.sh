4199a8fedbc984918a00f11dd773fb660ea6fe6f
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 23:03:23 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the comtent-lenght of a giving ip address
-curl -sI "$1" | awk '/Content-Length/{print $2}'
+#Bash script that takes in a URL, sends a request to that URL
+curl -s -o /dev/null -w "%{size_download}" "$1"
