5f440d6e3ecba974a96ce6d8841a2518efab30c8
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-03-05 01:24:27 +0100

    py network 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to get the body size of a request
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
